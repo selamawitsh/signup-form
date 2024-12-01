20871f9f1fd4d54fb5693922482e02bdfa0a3452
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2024-12-01 13:20:52 +0300

    update the js file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-document.getElementById('create-account').addEventListener('click',function(e){
+document.getElementById('create-account').addEventListener('click', function (e) {
     e.preventDefault();
-    const password=document.getElementById('password').value;
-    console.log(password)
-    const confirmPassword=document.getElementById('confirm-password').value;
-    console.log(confirmPassword)
-    const errmes=document.getElementById('errMessage');
 
-    
-    if(password==="" && confirmPassword===""){
-        errmes.textContent="Please provide a valid password."
-    }
-    else{
-        errmes.textContent="Password do not match!"
-    }
-    if (password===confirmPassword && password!=="" && confirmPassword!==""){
+    const password = document.getElementById('password').value;
+    console.log(password);
+    const confirmPassword = document.getElementById('confirm-password').value;
+    console.log(confirmPassword);
+    const errmes = document.getElementById('errMessage');
+
+    if (password === "" || confirmPassword === "") {
+        errmes.textContent = "Please provide a valid password.";
+    } 
+    else if (password !== confirmPassword) {
+        errmes.textContent = "Passwords do not match!";
+    } 
+    else {
+        errmes.textContent = "";
         window.location.href = 'thankyou.html';
     }
-    
 });
